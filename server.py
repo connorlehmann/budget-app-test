25605210fee932ec80391f9f85c92bbcf92323f1
Connor Lehmann <lehmannjconnor@gmail.com>
2026-07-18 17:33:52 -0400

    testing if the date will display

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 from flask import Flask, render_template, redirect, url_for
 from forms import AddExpenseForm, DeleteExpenseForm, NextBillForm
@@ -88,11 +88,23 @@ def nextbill():
     else:
         expense_list = []
 
+    tapered_expenses = [expense for expense in expense_list if expense["bill_type"] in ["monthly", "weekly"]]
+
     if form.validate_on_submit():
         name = form.name.data
-        
 
-        return redirect(url_for('home'))
+        for item in tapered_expenses:
+            if item["name"] == name:
+                base_date = datetime.strptime(item["date"], "%m/%d/%Y")
+                if item["bill_type"] == "monthly":
+                    next_date = base_date + timedelta(days=30)
+
+                elif item["bill_type"] == "weekly":
+                    next_date = base_date + timedelta(days=7)
+
+
+
+        return redirect(url_for('nextbill'))
 
     return render_template('NextBill.html', title='Regular', form=form)
 
